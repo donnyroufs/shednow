2a08175049fc1cf820df056bc23703843862188a
donny <48981403+donnyroufs@users.noreply.github.com>
2023-02-18 12:24:31 +0100

    feat(ui): add temporary audio player

--- packages/ui/src/app/features/view-posts/post.tsx
+++ packages/ui/src/app/features/view-posts/post.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Heading, Link, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Heading, Link, Text } from "@chakra-ui/react";
+import { useRef } from "react";
 
 type Props = {
   id: string;
@@ -8,24 +9,41 @@ type Props = {
   avatarUrl?: string;
 };
 
-export function Post({ id, title, authorName, avatarUrl }: Props) {
+export function Post({ id, title, authorName, avatarUrl, url }: Props) {
+  const ref = useRef<HTMLAudioElement>(null!);
+
+  function onPlay() {
+    if (ref.current.duration > 0 && !ref.current.paused) {
+      ref.current.pause();
+      return;
+    }
+
+    ref.current.play();
+  }
+
   return (
     <Box
       bgColor="#171C25"
       border="1px solid"
       display="flex"
+      flexDir="column"
       borderColor="#2E3542"
       p={8}
       borderRadius={8}
       alignItems="center"
     >
-      <Box>
-        <Avatar name={authorName} src={avatarUrl} />
+      <Box w="100%" display="flex" alignItems="center">
+        <Box>
+          <Avatar name={authorName} src={avatarUrl} />
+        </Box>
+        <Box ml={4}>
+          <Heading as="h2" fontSize="lg" textTransform="capitalize">
+            {title}
+          </Heading>
+        </Box>
       </Box>
-      <Box ml={4}>
-        <Heading as="h2" fontSize="lg" textTransform="capitalize">
-          {title}
-        </Heading>
+      <Box w="100%" mt={4}>
+        <audio src={url} controls ref={ref} />
       </Box>
     </Box>
   );
